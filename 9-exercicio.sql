08ecf5c64c5e39d40088b11bc07b750c87f2d1d7
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-02-09 20:52:10 -0300

    ex. 9 faturamento por mês

--- 9-exercicio.sql
+++ 9-exercicio.sql
@@ -1,1 +1,4 @@
 -- 9.	Mostre quanto foi faturado por mês (leve em conta o valor total de cada pedido - novamente pense em GROUP BY e SUM).
+
+select year(data_pedido) as 'Ano', monthname(data_pedido) as 'Mês', sum(valor_final) as 'Faturamento Mensal' from pedido
+	group by year(data_pedido), month(data_pedido);
