a658a13351a022af8ca488e53b996afdfbdaa548
allan alex <suportemg1@gmail.com>
2022-02-10 16:37:53 -0300

    resolução exercicios 5 e 6

--- 6-exercicio.sql
+++ 6-exercicio.sql
@@ -1,1 +1,10 @@
 -- 6.	Quantos produtos há em cada departamento? Exiba o nome do departamento e a quantidade de produtos que há em cada um. (pense em SUM e GROUP BY)
+select departamento.nome, count(produto.codigo) from departamento inner join produto 
+ON produto.departamento_codigo = departamento.codigo group by departamento.codigo;
+
+-- nome	count(produto.codigo)
+-- Administrativo	10
+-- Comercial	5
+-- Marketing	12
+-- Operacional	6
+
