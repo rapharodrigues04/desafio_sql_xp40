f318fe87fe0f148fe100fda14a840b2bfc616511
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-10 16:04:14 -0300

    adição dos exercícios 7 e 8

--- 7-exercicio.sql
+++ 7-exercicio.sql
@@ -1,1 +1,8 @@
 -- 7.	Mostre os dados dos pedidos, incluindo nomes dos clientes e nomes dos produtos que foram vendidos.
+
+SELECT cliente.nome, pedido.cliente_id 
+from cliente 
+inner join pedido
+on cliente.id = pedido.cliente_id;
+
+-- Nos pedidos, não constam os nomes dos produtos, por isso essa informação não é mostrada aqui
