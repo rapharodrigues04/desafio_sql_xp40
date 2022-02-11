8c3a292360a9e3555a9e1de66080f45c2f2595c9
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2022-02-10 22:51:30 -0300

    Ajuste no select do exercicio 7

--- 7-exercicio.sql
+++ 7-exercicio.sql
@@ -1,8 +1,6 @@
 -- 7.	Mostre os dados dos pedidos, incluindo nomes dos clientes e nomes dos produtos que foram vendidos.
 
-SELECT cliente.nome, pedido.cliente_id 
-from cliente 
-inner join pedido
-on cliente.id = pedido.cliente_id;
-
--- Nos pedidos, não constam os nomes dos produtos, por isso essa informação não é mostrada aqui
+SELECT * FROM pedido 
+	INNER JOIN item_pedido ON pedido.numero = item_pedido.pedido_numero
+    INNER JOIN cliente ON pedido.cliente_id = cliente.id
+	INNER JOIN produto ON produto.codigo = item_pedido.produto_codigo
